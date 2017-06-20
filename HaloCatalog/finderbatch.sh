8aa7666be4d7f1ad6db89b2ad0b406e9acb637eb
Yashodhar Govil <ygovil@princeton.edu>
2017-06-20 10:03:32 -0400

    tried out batch script again

--- HaloCatalog/finderbatch.sh
+++ HaloCatalog/finderbatch.sh
@@ -1,3 +1,11 @@
-#!/usr/bin
-#SBATCH --time=1
-srun python prelim_halo_finder.py
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p regular
+#SBATCH -J findhalos
+#SBATCH --output=test.txt
+#SBATCH --mail-user=ygovil@princeton.edu
+#SBATCH --mail-type=ALL
+#SBATCH -t 00:01:00
+
+#run the application:
+srun -n 1  ~/repositories/data-mining-galaxies/HaloCatalog/prelim_halo_finder.py
