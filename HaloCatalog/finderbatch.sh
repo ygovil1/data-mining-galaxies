335a3605f25f030d91cb3044d6180e89795648cc
Yashodhar Govil <ygovil@princeton.edu>
2017-06-20 11:01:04 -0400

    fixed batch script issues, need to increase time allotted

--- HaloCatalog/finderbatch.sh
+++ HaloCatalog/finderbatch.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -p regular
 #SBATCH -J findhalos
-#SBATCH --output=test.txt
 #SBATCH --mail-user=ygovil@princeton.edu
 #SBATCH --mail-type=ALL
-#SBATCH -t 00:01:00
+#SBATCH -t 00:10:00
 
 #run the application:
-srun -n 1  ~/repositories/data-mining-galaxies/HaloCatalog/prelim_halo_finder.py
+srun -n 1 python ~/repositories/data-mining-galaxies/HaloCatalog/prelim_halo_finder.py
