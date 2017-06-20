9ec7a8957cb350ac34f3589dc0deea5b1adba351
Yashodhar Govil <ygovil@princeton.edu>
2017-06-20 11:05:55 -0400

    tidy up slurm outputs

--- HaloCatalog/finderbatch.sh
+++ HaloCatalog/finderbatch.sh
@@ -3,7 +3,7 @@
 #SBATCH -J findhalos
 #SBATCH --mail-user=ygovil@princeton.edu
 #SBATCH --mail-type=ALL
-#SBATCH -t 00:10:00
+#SBATCH -t 00:09:00
 
 #run the application:
 srun -n 1 python ~/repositories/data-mining-galaxies/HaloCatalog/prelim_halo_finder.py
