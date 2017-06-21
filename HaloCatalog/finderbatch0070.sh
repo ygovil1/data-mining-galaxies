32b0954fe90f040c2ac696a5b8ee5b2c09b1be20
Yashodhar Govil <ygovil@princeton.edu>
2017-06-21 18:08:26 -0400

    changed names to be easier to differenciate

--- HaloCatalog/finderbatch0070.sh
+++ HaloCatalog/finderbatch0070.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -J findhalos
+#SBATCH -J findhalos0070
 #SBATCH --mail-user=ygovil@princeton.edu
 #SBATCH --mail-type=ALL
 #SBATCH -t 05:50:00
--- HaloCatalog/finderbatch0110.sh
+++ HaloCatalog/finderbatch0110.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -J findhalos
+#SBATCH -J findhalos0110
 #SBATCH --mail-user=ygovil@princeton.edu
 #SBATCH --mail-type=ALL
-#SBATCH -t 23:59:00
+#SBATCH -t 30:00:00
 #SBATCH --mem=60G
 
 #run the application:
