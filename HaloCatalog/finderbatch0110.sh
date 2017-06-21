2bc38da945b18feefa8041afbe14e2823d316828
Yashodhar Govil <ygovil@princeton.edu>
2017-06-21 13:40:41 -0400

    changed batch script to request 5:50hr and 16G, moved old slurm outputs

--- HaloCatalog/finderbatch0110.sh
+++ HaloCatalog/finderbatch0110.sh
@@ -3,8 +3,8 @@
 #SBATCH -J findhalos
 #SBATCH --mail-user=ygovil@princeton.edu
 #SBATCH --mail-type=ALL
-#SBATCH -t 04:00:00
-#SBATCH --mem=5G
+#SBATCH -t 05:50:00
+#SBATCH --mem=16G
 
 #run the application:
 srun -n 1 python ~/repositories/data-mining-galaxies/HaloCatalog/halo_finder0110.py
