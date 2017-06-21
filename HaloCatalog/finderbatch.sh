41dd6b79838cd2c1f63cb6bc112db3db67e5e43b
Yashodhar Govil <ygovil@princeton.edu>
2017-06-21 09:54:27 -0400

    modified batch script to ask for 5G mem

--- HaloCatalog/finderbatch.sh
+++ HaloCatalog/finderbatch.sh
@@ -4,6 +4,7 @@
 #SBATCH --mail-user=ygovil@princeton.edu
 #SBATCH --mail-type=ALL
 #SBATCH -t 04:00:00
+#SBATCH --mem=5G
 
 #run the application:
 srun -n 1 python ~/repositories/data-mining-galaxies/HaloCatalog/prelim_halo_finder.py
