0b56cd2aa454efe3bc0f29dc7f97c16888ab623a
Yashodhar Govil <ygovil@princeton.edu>
2017-06-22 09:53:08 -0400

    changed time to 5hr and mem to 12G

--- HaloCatalog/finderbatch0110.sh
+++ HaloCatalog/finderbatch0110.sh
@@ -3,8 +3,8 @@
 #SBATCH -J findhalos0110
 #SBATCH --mail-user=ygovil@princeton.edu
 #SBATCH --mail-type=ALL
-#SBATCH -t 30:00:00
-#SBATCH --mem=60G
+#SBATCH -t 05:00:00
+#SBATCH --mem=12G
 
 #run the application:
 srun -n 1 python ./RD0110/halo_finder0110.py
