61645f88dc434681e1e49bf30c7552e9f14042fc
Yashodhar Govil <ygovil@princeton.edu>
2017-06-23 16:28:23 -0400

    minor changes

--- HaloCatalog/LG4_2048_40pc/rd0070/make_graph0070.py
+++ HaloCatalog/LG4_2048_40pc/rd0070/make_graph0070.py
@@ -1,1 +1,9 @@
-import 
+# import basic libraries
+import yt
+import numpy as np
+import matplotlib.pyplot as plt
+
+# import halo catalogue func
+from yt.analysis_modules.halo_analysis.api import *
+
+
