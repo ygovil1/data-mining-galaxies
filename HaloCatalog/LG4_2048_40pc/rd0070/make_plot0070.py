8a555a606188a90ff495302340fbd230a8639ea9
Yashodhar Govil <ygovil@princeton.edu>
2017-06-23 14:03:39 -0400

    change some parameter

--- HaloCatalog/LG4_2048_40pc/rd0070/make_plot0070.py
+++ HaloCatalog/LG4_2048_40pc/rd0070/make_plot0070.py
@@ -16,9 +16,14 @@ halos_ds = yt.load('./halo_catalogs/catalog/catalog0070.0.h5')
 # Instantiate a catalog using those two paramter files
 hc = HaloCatalog(data_ds=ds, halos_ds=halos_ds, output_dir=os.path.join(tmpdir, 'halo_catalog'))
 
+# Filter out less massive halos
+hc.add_filter("quantity_value", "particle_mass", ">", 1e14, "Msun")
+
+hc.load()
+
 # create projection plot
-p = yt.ProjectionPlot(ds, "x", "Dark_Matter_Density", width=(1.5, 'Mpc'), max_level=20)
-p.set_zlim(field="Dark_Matter_Density", zmin=5e24, zmax=1e26)
+p = yt.ProjectionPlot(ds, "x", "Dark_Matter_Density", width=(1.5, 'Mpc'), max_level=30)
+p.set_zlim(field="Dark_Matter_Density", zmin=5e24, zmax=2e26)
 #p.annotate_halos(hc, factor = "particle_mass")
 p.annotate_halos(hc)
 p.save("./projplot0070.png")
