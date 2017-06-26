a5dca696d4b995f6089b6111881ebee79a92df3a
Yashodhar Govil <ygovil@princeton.edu>
2017-06-26 15:31:27 -0400

    remove tempdir portion

--- HaloCatalog/LG4_2048_40pc/rd0070/make_plot0070.py
+++ HaloCatalog/LG4_2048_40pc/rd0070/make_plot0070.py
@@ -1,11 +1,5 @@
 import yt
 from yt.analysis_modules.halo_analysis.api import *
-import tempfile
-import shutil
-import os
-
-# Create temporary directory for storing files
-tmpdir = tempfile.mkdtemp()
 
 # create dataset
 ds = yt.load('~/../../tigress/cen/LG4_2048_40pc/RD0070/redshift0070')
@@ -14,12 +8,12 @@ ds = yt.load('~/../../tigress/cen/LG4_2048_40pc/RD0070/redshift0070')
 halos_ds = yt.load('./halo_catalogs/catalog/catalog0070.0.h5')
 
 # Instantiate a catalog using those two paramter files
-hc = HaloCatalog(data_ds=ds, halos_ds=halos_ds, output_dir=os.path.join(tmpdir, 'halo_catalog'))
+hc = HaloCatalog(data_ds=ds, halos_ds=halos_ds)
 
 # Filter out less massive halos
-hc.add_filter("quantity_value", "particle_mass", ">", 1e10, "Msun")
+hc.add_filter("quantity_value", "particle_mass", ">", 1e14, "Msun")
 
-hc.create(save_catalog=False)
+hc.load()
 
 # create projection plot
 p = yt.ProjectionPlot(ds, "x", "Dark_Matter_Density", width=(1.5, 'Mpc'), max_level=30)
