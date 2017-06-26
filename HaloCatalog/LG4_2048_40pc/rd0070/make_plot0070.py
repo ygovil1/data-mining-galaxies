3f78c3e1ca14a22a2b085891030e66e7d3822474
Yashodhar Govil <ygovil@princeton.edu>
2017-06-26 16:19:29 -0400

    minor changes, may fix bug

--- HaloCatalog/LG4_2048_40pc/rd0070/make_plot0070.py
+++ HaloCatalog/LG4_2048_40pc/rd0070/make_plot0070.py
@@ -11,9 +11,8 @@ halos_ds = yt.load('./halo_catalogs/catalog/catalog0070_thres160.0.h5')
 hc = HaloCatalog(data_ds=ds, halos_ds=halos_ds)
 
 # Filter out less massive halos
-hc.add_filter("quantity_value", "particle_mass", ">", 1e14, "Msun")
-
-hc.load()
+hc.add_filter("quantity_value", "particle_mass", ">", 1e8, "Msun")
+hc = hc.load()
 
 # create projection plot
 p = yt.ProjectionPlot(ds, "x", "Dark_Matter_Density", width=(1.5, 'Mpc'), max_level=30)
