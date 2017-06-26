a069005157d849f5e69321275a4ba46cb8abbe77
Yashodhar Govil <ygovil@princeton.edu>
2017-06-26 15:17:34 -0400

    changed threshold 160->180

--- HaloCatalog/LG4_2048_40pc/rd0070/halo_finder0070.py
+++ HaloCatalog/LG4_2048_40pc/rd0070/halo_finder0070.py
@@ -11,7 +11,7 @@ from yt.analysis_modules.halo_finding.halo_objects import HaloFinder
 ds = yt.load('~/../../tigress/cen/LG4_2048_40pc/RD0070/redshift0070')
 
 # specify parameters for halo finder
-finder_pars = {'threshold': 160., 'dm_only':True, 'padding' : 0.02}
+finder_pars = {'threshold': 180., 'dm_only':True, 'padding' : 0.02}
 
 # create halo catalogue
 hc = HaloCatalog(finder_kwargs=finder_pars, data_ds=ds, finder_method='hop')
