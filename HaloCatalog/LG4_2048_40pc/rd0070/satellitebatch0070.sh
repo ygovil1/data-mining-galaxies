79786a3786584287343c8c025163d381875616e1
Yashodhar Govil <ygovil@princeton.edu>
2017-06-30 10:23:05 -0400

    chnaged to run satellite finder v2

--- HaloCatalog/LG4_2048_40pc/rd0070/satellitebatch0070.sh
+++ HaloCatalog/LG4_2048_40pc/rd0070/satellitebatch0070.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -J satellitefinderLG40070
+#SBATCH -J satellitefinderv2LG40070
 #SBATCH --mail-user=ygovil@princeton.edu
 #SBATCH --mail-type=ALL
-#SBATCH -t 3:30:00
+#SBATCH -t 0:59:00
 #SBATCH --mem=4G
 
 #run the application:
-srun -n 1 python ./satellite_finder0070.py
+srun -n 1 python ./satellite_finder0070v2.py
