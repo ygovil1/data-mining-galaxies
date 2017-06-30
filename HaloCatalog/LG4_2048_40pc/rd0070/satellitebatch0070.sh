3f48af54c4f3438f0a30fc0da4bcb6d0dca4b393
Yashodhar Govil <ygovil@princeton.edu>
2017-06-30 12:06:31 -0400

    changed satellite finder allocation to 3hr and 8G

--- HaloCatalog/LG4_2048_40pc/rd0070/satellitebatch0070.sh
+++ HaloCatalog/LG4_2048_40pc/rd0070/satellitebatch0070.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-user=ygovil@princeton.edu
 #SBATCH --mail-type=ALL
 #SBATCH -t 3:00:00
-#SBATCH --mem=4G
+#SBATCH --mem=8G
 
 #run the application:
 srun -n 1 python ./satellite_finder0070v2.py
