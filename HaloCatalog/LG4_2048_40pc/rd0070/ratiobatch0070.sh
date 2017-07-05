e44b31fa7eabf8131eaf67ca199ef2065432ff78
Yashodhar Govil <ygovil@princeton.edu>
2017-07-05 14:43:22 -0400

    find radii

--- HaloCatalog/LG4_2048_40pc/rd0070/ratiobatch0070.sh
+++ HaloCatalog/LG4_2048_40pc/rd0070/ratiobatch0070.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -J ratiofinderLG40070
+#SBATCH -J rad_ratiofinderLG40070
 #SBATCH --mail-user=ygovil@princeton.edu
 #SBATCH --mail-type=ALL
-#SBATCH -t 2:30:00
-#SBATCH --mem=2G
+#SBATCH -t 4:30:00
+#SBATCH --mem=20G
 
 #run the application:
-srun -n 1 python ./ratio_finder0070.py
+srun -n 1 python ./rad_ratiofinder0070.py
