1854dfe1d12937a4bc9d4d34646392b4b4ab1507
Yashodhar Govil <ygovil@princeton.edu>
2017-07-10 21:35:03 -0400

    create files to find halos incrementally

---
+++ HaloCatalog/LG4_2048_40pc/rd0070/calc_halo/new_halobatch0070_100_200.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -J new_radiusfinderLG40070
+#SBATCH --mail-user=ygovil@princeton.edu
+#SBATCH --mail-type=ALL
+#SBATCH -t 00:59:00
+#SBATCH --mem=20G
+
+#run the application:
+srun -n 1 python ./new_radiusfinder0070_100_200.py
