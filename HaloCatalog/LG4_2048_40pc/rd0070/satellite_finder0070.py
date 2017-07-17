4438cdf13aa8b7b85bdcbc9f22d31e69c7890c31
Yashodhar Govil <ygovil@princeton.edu>
2017-07-17 14:12:10 -0400

    modify satellite finding code to use calc_list

--- HaloCatalog/LG4_2048_40pc/rd0070/satellite_finder0070.py
+++ HaloCatalog/LG4_2048_40pc/rd0070/satellite_finder0070.py
@@ -1,14 +1,19 @@
+#specify input filenames
+halo_catalog_filename = './halo_catalogs/catalog/catalog0070_thres160.0.h5'
+dataset_filename = '~/../../tigress/cen/LG4_2048_40pc/RD0070/redshift0070'
+redshift_filename = 'redshift0070'
+
 # import basic libraries
-import yt
 import pickle
+import yt
 import numpy as np
 import matplotlib.pyplot as plt
+from math import log, log10, pi
 from astropy import units as u
+from operator import itemgetter
 
-# import halo catalogue func
-from yt.analysis_modules.halo_analysis.api import *
-
-# import functions to ensure hc works
+# import libraries - not sure what they do
+# used to ensure halo catalog loads properly
 import tempfile
 import shutil
 import os
@@ -16,95 +21,164 @@ import os
 # Create temporary directory for storing files
 tmpdir = tempfile.mkdtemp()
 
-# load data dataset
-# ds = yt.load('~/../../tigress/cen/LG4_2048_40pc/RD0070/redshift0070')
+# import halo catalogue func
+from yt.analysis_modules.halo_analysis.api import *
 
 # load halo dataset
-halos_ds = yt.load('./halo_catalogs/catalog/catalog0070_thres160.0.h5')
+halos_ds = yt.load(halo_catalog_filename)
+
+# load raw dataset
+ds = yt.load(dataset_filename)
 
 # Instantiate a catalog using those two paramter files
 hc = HaloCatalog(halos_ds=halos_ds, output_dir=os.path.join(tmpdir, 'halo_catalog'))
 hc.load()
 
+# load python halo list
+with open('halo_list0070', 'rb') as file1:
+    halo_list = pickle.load(file1)
+
+# load mass of smallest halo
+with open('finest_particle0070', 'rb') as file2:
+    min_mass = pickle.load(file2)
+    min_mass = 100 * min_mass * u.g # 100*finest_particle
+
+# load halo count from file
+with open('count_halo0070_160', 'rb') as file3:
+    count = pickle.load(file3)
+    
+# load redshift and Omega values from parameter file
+with open(redshift_filename, 'rt') as param_file:
+    param_contents = param_file.read()
+    
+    #redshift
+    cindex1 = param_contents.find('CosmologyCurrentRedshift')
+    cindex_eq = param_contents.find('=', cindex1)
+    cindex2 = param_contents.find('\n', cindex_eq, cindex_eq + 100)
+    redshift = float(param_contents[cindex_eq+2:cindex2])
+    
+    # omega_m
+    cindex1 = param_contents.find('CosmologyOmegaMatterNow')
+    cindex_eq = param_contents.find('=', cindex1)
+    cindex2 = param_contents.find('\n', cindex_eq, cindex_eq + 100)
+    omega_m = float(param_contents[cindex_eq+2:cindex2])
+    
+    # hubble const now
+    cindex1 = param_contents.find('CosmologyHubbleConstantNow')
+    cindex_eq = param_contents.find('=', cindex1)
+    cindex2 = param_contents.find('\n', cindex_eq, cindex_eq + 100)
+    hubb_now = float(param_contents[cindex_eq+2:cindex2]) *(u.km / u.s / u.Mpc)
+    hubb_now = hubb_now.to('s**-1')
+    
+    # cosmological constant
+    cindex1 = param_contents.find('CosmologyOmegaLambdaNow')
+    cindex_eq = param_contents.find('=', cindex1)
+    cindex2 = param_contents.find('\n', cindex_eq, cindex_eq + 100)
+    cos_const = float(param_contents[cindex_eq+2:cindex2])
+    
+    # omega baryon as specified by Renyue
+    omega_b = 0.048
+    
+    # box size
+    cindex1 = param_contents.find('CosmologyComovingBoxSize')
+    cindex_eq = param_contents.find('=', cindex1)
+    cindex2 = param_contents.find('\n', cindex_eq, cindex_eq + 100)
+    box_size = float(param_contents[cindex_eq+2:cindex2]) * u.Mpc
+    
+    # left edge strings
+    cindex1 = param_contents.find('RefineRegionLeftEdge')
+    cindex_eq = param_contents.find('=', cindex1)
+    cindex2 = param_contents.find('\n', cindex_eq, cindex_eq + 100)
+    left_edge_string = param_contents[cindex_eq+2:cindex2]
+    left_edges = left_edge_string.split()
+    
+    # right edge strings
+    cindex1 = param_contents.find('RefineRegionRightEdge')
+    cindex_eq = param_contents.find('=', cindex1)
+    cindex2 = param_contents.find('\n', cindex_eq, cindex_eq + 100)
+    right_edge_string = param_contents[cindex_eq+2:cindex2]
+    right_edges = right_edge_string.split()
+    
+# calculate hubble const for simulation
+hubb_z = (100* hubb_now) * ((omega_m * (1 + redshift)**3) + (1 - omega_m))**0.5
+
 # specify boundaries of zoom-in box
-# scaling factor multiplied by info from text file
+# scaling factor multiplied by info from text file 
 # units in cm
-scaling = 2.22535525e+25 # scales dataset coords to cm
+scaling =  ((box_size / hubb_now.value) / (1 + redshift)).to('cm') # size of box
+xmin = scaling*float(left_edges[0])
+ymin = scaling*float(left_edges[1])
+zmin = scaling*float(left_edges[2])
+xmax = scaling*float(right_edges[0])
+ymax = scaling*float(right_edges[1])
+zmax = scaling*float(right_edges[2])
 
-xmin = scaling*0.39319589 * u.cm
-ymin = scaling*0.42984636 * u.cm
-zmin = scaling*0.41706725 * u.cm
-
-xmax = scaling*0.56298484 * u.cm
-ymax = scaling*0.55089246 * u.cm
-zmax = scaling*0.56698254 * u.cm
-
-# create lists for satellites and non-satellites
-satellite = []
-nonsatellite = []
+# set min mass to be 8e6 Msun
+min_mass = 8e6 * u.Msun
 
 # fill up lists
-for halo1 in hc.halo_list:
+new_halo_list = []
+for halo1 in halo_list:
     # find first index
-    index1 = halo1.quantities.get('particle_identifier')
+    index1 = halo1[0]
 
-    # create bool to check if this halo isn't satellite
-    isSatellite = False
+    # create int to check if this halo isn't satellite
+    isSatellite = -1
 
     # find parameters of halo1
-    x1 = halo1.quantities.get('particle_position_x')
-    y1 = halo1.quantities.get('particle_position_y')
-    z1 = halo1.quantities.get('particle_position_z')
-    radius1 = halo1.quantities.get('virial_radius')
+    x1 = halo1[2]
+    y1 = halo1[3]
+    z1 = halo1[4]
+    mass1 = halo1[5]
+    radius1 = halo1[7]
 
     # loop through second halo
-    for halo2 in hc.halo_list:
+    for halo2 in halo_list:
         # find second index
-        index2 = halo1.quantities.get('particle_identifier')
-
-        if index2 <= index1:
+        index2 = halo2[0]
+        
+        # if same halo, skip
+        if index1 == index2:
             continue
 
         # find parameters of halo2
-        x2 = halo2.quantities.get('particle_position_x')
-        y2 = halo2.quantities.get('particle_position_y')
-        z2 = halo2.quantities.get('particle_position_z')
-        radius2 = halo2.quantities.get('virial_radius')
+        x2 = halo2[2]
+        y2 = halo2[3]
+        z2 = halo2[4]
+        mass2 = halo2[5]
+        radius2 = halo2[7]
+        
+        # if error with new rad (too large, too small, didn't cross threshold) 
+        # skip this halo
+        if radius2 > (40 * u.kpc) or radius2 == 0:
+            continue
 
         # calculate dist between halo centers
-        xdist = np.absolute(x2 - x1)
-        ydist = np.absolute(y2 - y1)
-        zdist = np.absolute(z2 - z1)
+        xdist = x2 - x1
+        ydist = y2 - y1
+        zdist = z2 - z1
         netdist = (xdist**2 + ydist**2 + zdist**2)**0.5
+        
+        # If already a satellite of a halo, skip all future halos
+        # this is because halos are ordered by decreasing mass
+        if isSatellite >= 0:
+            continue
 
-        # if netdist < both radii, halo with smaller radius is satellite
-        if netdist < radius1 and netdist < radius2:
-            # check which radius smaller, if so, add index to list
-            if radius2 < radius1:
-                satellite.append(index2)
-            else:
-                satellite.append(index1)
-                # set bool to true for later
-                isSatellite = True
+        # if halo1 inside halo2, halo1 satellite of halo2
+        if netdist < radius2:
+            isSatellite = index2
 
-        # if netdist only < rad1, halo2 is satellite
-        elif netdist < radius1:
-            satellite.append(index2)
+    # after loop, append isSatellite int to halo_info
+    # ensure that running this code multiple times doesn't ceate a long list
+    new_halo = halo1[:9]
+    new_halo.append(isSatellite)
+    
+    # print result
+    print(new_halo)
+    
+    # append halo_info to new halo list
+    new_halo_list.append(new_halo)
 
-        # if netdist only < rad2, halo 1 is satellite
-        elif netdist < radius2:
-            satellite.append(index1)
-            # set bool to true for later
-            isSatellite = True
-
-    # after loop, if satellite bool still false, and halo1 not in satellite
-    # list, then add to non-satellite list
-    if not isSatellite and index1 not in satellite:
-        nonsatellite.append(index1)
-
-# store lists in files
-with open('satellite_list0070.txt', 'wb') as satellite_file:
-    pickle.dump(satellite, satellite_file)
-
-with open('satellite_non_list0070.txt', 'wb') as nonsatellite_file:
-    pickle.dump(nonsatellite, nonsatellite_file)
+# store new list to file
+with open('calc_list0070_2000', 'wb') as outfile:
+    pickle.dump(new_halo_list, outfile)
--- HaloCatalog/LG4_2048_40pc/rd0070/satellitebatch0070.sh
+++ HaloCatalog/LG4_2048_40pc/rd0070/satellitebatch0070.sh
@@ -3,8 +3,8 @@
 #SBATCH -J satellitefinderv2LG40070
 #SBATCH --mail-user=ygovil@princeton.edu
 #SBATCH --mail-type=ALL
-#SBATCH -t 3:00:00
-#SBATCH --mem=8G
+#SBATCH -t 00:59:00
+#SBATCH --mem=4G
 
 #run the application:
-srun -n 1 python ./satellite_finder0070v2.py
+srun -n 1 python ./satellite_finder0070.py
