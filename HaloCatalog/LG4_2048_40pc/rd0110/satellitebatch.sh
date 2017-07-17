bc61ea6f39f2e725ab08afc6d33de0996893f0d7
Yashodhar Govil <ygovil@princeton.edu>
2017-07-17 15:29:32 -0400

    added files for rest of snapshots

---
+++ HaloCatalog/LG4_2048_40pc/rd0110/satellitebatch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -J satellitefinderv2LG40110
+#SBATCH --mail-user=ygovil@princeton.edu
+#SBATCH --mail-type=ALL
+#SBATCH -t 00:59:00
+#SBATCH --mem=4G
+
+#run the application:
+srun -n 1 python ./satellite_finder.py
