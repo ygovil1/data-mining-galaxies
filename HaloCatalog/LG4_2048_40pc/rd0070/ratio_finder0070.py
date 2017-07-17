480b67474280c4816a2a1afcdc5b39832559126e
Yashodhar Govil <ygovil@princeton.edu>
2017-07-17 14:10:34 -0400

    modified ratio finder to remove bugs

--- HaloCatalog/LG4_2048_40pc/rd0070/ratio_finder0070.py
+++ HaloCatalog/LG4_2048_40pc/rd0070/ratio_finder0070.py
@@ -1,3 +1,8 @@
+#specify input filenames
+halo_catalog_filename = './halo_catalogs/catalog/catalog0070_thres160.0.h5'
+dataset_filename = '~/../../tigress/cen/LG4_2048_40pc/RD0070/redshift0070'
+redshift_filename = 'redshift0070'
+
 # import basic libraries
 import pickle
 import yt
@@ -5,6 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import log, log10, pi
 from astropy import units as u
+from operator import itemgetter
 
 # import libraries - not sure what they do
 # used to ensure halo catalog loads properly
@@ -19,10 +25,10 @@ tmpdir = tempfile.mkdtemp()
 from yt.analysis_modules.halo_analysis.api import *
 
 # load halo dataset
-halos_ds = yt.load('./halo_catalogs/catalog/catalog0070_thres160.0.h5')
+halos_ds = yt.load(halo_catalog_filename)
 
 # load raw dataset
-ds = yt.load('~/../../tigress/cen/LG4_2048_40pc/RD0070/redshift0070')
+ds = yt.load(dataset_filename)
 
 # Instantiate a catalog using those two paramter files
 hc = HaloCatalog(halos_ds=halos_ds, output_dir=os.path.join(tmpdir, 'halo_catalog'))
@@ -42,7 +48,7 @@ with open('count_halo0070_160', 'rb') as file3:
     count = pickle.load(file3)
     
 # load redshift and Omega values from parameter file
-with open('redshift0070', 'rt') as param_file:
+with open(redshift_filename, 'rt') as param_file:
     param_contents = param_file.read()
     
     #redshift
@@ -61,8 +67,7 @@ with open('redshift0070', 'rt') as param_file:
     cindex1 = param_contents.find('CosmologyHubbleConstantNow')
     cindex_eq = param_contents.find('=', cindex1)
     cindex2 = param_contents.find('\n', cindex_eq, cindex_eq + 100)
-    hubb_now = float(param_contents[cindex_eq+2:cindex2]) * 100 *(u.km / u.s / u.Mpc)
-    hubb_now = hubb_now.to('s**-1')
+    hubb_now = float(param_contents[cindex_eq+2:cindex2]) *(u.km / u.s / u.Mpc) 
     
     # cosmological constant
     cindex1 = param_contents.find('CosmologyOmegaLambdaNow')
@@ -73,8 +78,28 @@ with open('redshift0070', 'rt') as param_file:
     # omega baryon as specified by Renyue
     omega_b = 0.048
     
+    # box size
+    cindex1 = param_contents.find('CosmologyComovingBoxSize')
+    cindex_eq = param_contents.find('=', cindex1)
+    cindex2 = param_contents.find('\n', cindex_eq, cindex_eq + 100)
+    box_size = float(param_contents[cindex_eq+2:cindex2]) * u.Mpc
+    
+    # left edge strings
+    cindex1 = param_contents.find('RefineRegionLeftEdge')
+    cindex_eq = param_contents.find('=', cindex1)
+    cindex2 = param_contents.find('\n', cindex_eq, cindex_eq + 100)
+    left_edge_string = param_contents[cindex_eq+2:cindex2]
+    left_edges = left_edge_string.split()
+    
+    # right edge strings
+    cindex1 = param_contents.find('RefineRegionRightEdge')
+    cindex_eq = param_contents.find('=', cindex1)
+    cindex2 = param_contents.find('\n', cindex_eq, cindex_eq + 100)
+    right_edge_string = param_contents[cindex_eq+2:cindex2]
+    right_edges = right_edge_string.split()
+    
 # calculate hubble const for simulation
-hubb_z = hubb_now * ((omega_m * (1 + redshift)**3) + (1 - omega_m))**0.5
+hubb_z = (100* hubb_now.to('s**-1')) * ((omega_m * (1 + redshift)**3) + (1 - omega_m))**0.5
 
 # calculate crit density and threshold
 GRAV_CONST = (6.67408e-11 * u.m**3 /(u.kg * u.s**2)).to('cm^3*g^-1*s^-2')
@@ -93,13 +118,13 @@ rad_max = rad_max / (1 + redshift) # convert to physical
 # specify boundaries of zoom-in box
 # scaling factor multiplied by info from text file 
 # units in cm
-scaling = 2.22535525e+25 # scales dataset coords to cm
-xmin = scaling*0.39319589 * u.cm
-ymin = scaling*0.42984636 * u.cm
-zmin = scaling*0.41706725 * u.cm
-xmax = scaling*0.56298484 * u.cm
-ymax = scaling*0.55089246 * u.cm
-zmax = scaling*0.56698254 * u.cm
+scaling =  ((box_size / hubb_now.value) / (1 + redshift)).to('kpc') # size of box
+xmin = scaling*float(left_edges[0])
+ymin = scaling*float(left_edges[1])
+zmin = scaling*float(left_edges[2])
+xmax = scaling*float(right_edges[0])
+ymax = scaling*float(right_edges[1])
+zmax = scaling*float(right_edges[2])
 
 # --find ratio of stellar mass to halo mass
 # first list uses first output of TotalMass funct
@@ -107,14 +132,19 @@ zmax = scaling*0.56698254 * u.cm
 ratiolist1 = []
 ratiolist2 = []
 
+# update calc_halo list also
+new_halo_list = []
+
 for halo in halo_list:
     # find parameters of halo
+    index = halo[0]
     x = halo[2]
     y = halo[3]
     z = halo[4]
-    center = [x.value/scaling, y.value/scaling, z.value/scaling]
+    center = [(x/scaling).value, (y/scaling).value, (z/scaling).value]
     halo_mass = halo[5] # new mass
     radius = halo[7] # new radius
+    isSatellite = halo[9]
     
     # check if halo is inside zoom-in box
     if xmin <= x < xmax and ymin <= y < ymax and zmin <= z < zmax:
@@ -124,6 +154,28 @@ for halo in halo_list:
 
     # check that radius is not 0
     if radius == 0:
+        # append 0's to halo_info if data not applicable
+        # ensure that running this code multiple times doesn't ceate a long list
+        new_halo = halo[:10]
+        new_halo.append(0)
+        new_halo.append(0)
+
+        # append halo_info to new halo list
+        new_halo_list.append(new_halo)
+        # skip rest of tests
+        continue
+        
+    # check that not a satellite 
+    if isSatellite >= 0:
+        # append 0's to halo_info if data not applicable
+        # ensure that running this code multiple times doesn't ceate a long list
+        new_halo = halo[:10]
+        new_halo.append(0)
+        new_halo.append(0)
+
+        # append halo_info to new halo list
+        new_halo_list.append(new_halo)
+        # skip rest of tests
         continue
     
     # create a sphere data object with halo position and radius
@@ -137,23 +189,34 @@ for halo in halo_list:
     particle_mass = particle_mass.to('Msun')
 
     # find stellar mass using total particle mass from TotalMass
-    stellar_mass = particle_mass.to('Msun') - halo_mass.to('Msun')
+    stellar_mass = particle_mass.to('Msun') - (halo_mass*omegas).to('Msun')
 
     # find the two ratios
     ratio1 = gas_mass / halo_mass
     ratio2 = stellar_mass / halo_mass
 
-    print(gas_mass.value, particle_mass.value, stellar_mass.value)
-    print(x, y, z, '\n')
+    print(index, '\n', gas_mass.value, particle_mass.value, stellar_mass.value)
+    print(ratio1, ratio2, '\n')
     
     # add ratios to list
     ratiolist1.append(ratio1.value)
     ratiolist2.append(ratio2.value)
-
+    
+    # append calculated ratios to halo_info
+    # ensure that running this code multiple times doesn't ceate a long list
+    new_halo = halo[:10]
+    new_halo.append(ratio1.value)
+    new_halo.append(ratio2.value)
+    
+    # append halo_info to new halo list
+    new_halo_list.append(new_halo)
         
-# store lists in files
+# store ratio lists in files
 with open('ratio_list0070_1.txt', 'wb') as ratiofile1:
     pickle.dump(ratiolist1, ratiofile1)
-
 with open('ratio_list0070_2.txt', 'wb') as ratiofile2:
     pickle.dump(ratiolist2, ratiofile2)
+    
+# store new halo list to file
+with open('calc_list00070_2000', 'wb') as outfile:
+    pickle.dump(new_halo_list, outfile)
