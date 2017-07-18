1770dcc827febe31171a2582585e1537426172a5
Yashodhar Govil <ygovil@princeton.edu>
2017-07-18 15:03:33 -0400

    minor name/memory changes

--- HaloCatalog/LG4_2048_40pc/rd0070/ratiobatch0070.sh
+++ HaloCatalog/LG4_2048_40pc/rd0070/ratiobatch0070.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -J rad_ratiofinderLG40070
+#SBATCH -J ratiofinderLG40070
 #SBATCH --mail-user=ygovil@princeton.edu
 #SBATCH --mail-type=ALL
 #SBATCH -t 00:59:00
-#SBATCH --mem=2G
+#SBATCH --mem=20G
 
 #run the application:
 srun -n 1 python ./ratio_finder0070.py
