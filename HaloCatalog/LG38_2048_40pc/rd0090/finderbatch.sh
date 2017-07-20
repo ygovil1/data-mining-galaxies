0833ad1b966cc672a4b83a2378d3b7ae7e27ddac
Yashodhar Govil <ygovil@princeton.edu>
2017-07-20 17:22:18 -0400

    added files for LG38

---
+++ HaloCatalog/LG38_2048_40pc/rd0090/finderbatch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -J findhalosLG38.0090
+#SBATCH --mail-user=ygovil@princeton.edu
+#SBATCH --mail-type=ALL
+#SBATCH -t 05:58:00
+#SBATCH --mem=10G
+
+#run the applications
+srun -n 1 python ./halo_finder.py
+srun -n 1 python ./list_maker.py
+
