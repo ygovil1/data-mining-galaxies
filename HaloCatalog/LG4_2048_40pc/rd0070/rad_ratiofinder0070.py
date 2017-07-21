be6ef3af1c0490000ab25caad5a48c97fcc7b8b8
Yashodhar Govil <ygovil@princeton.edu>
2017-07-21 14:25:26 -0400

    repurpose rad_ratiofinder for third measure

--- HaloCatalog/LG4_2048_40pc/rd0070/rad_ratiofinder0070.py
+++ HaloCatalog/LG4_2048_40pc/rd0070/rad_ratiofinder0070.py
@@ -1,98 +1,176 @@
+#specify input filenames
+halo_catalog_filename = './halo_catalogs/catalog/catalog0070_thres160.0.h5'
+dataset_filename = '~/../../tigress/cen/LG4_2048_40pc/RD0070/redshift0070'
+redshift_filename = 'redshift0070'
+
+# some constants
+start_num_entries = 13 # number of entries halos have at start of this program
+end_num_entries = 14 # min number of entries halos have at end of this program
+# note- start_num_entires is index of first changed/appended value
+
 # import basic libraries
 import pickle
 import yt
 import numpy as np
 import matplotlib.pyplot as plt
-from math import log, log10
+from math import log, log10, pi
 from astropy import units as u
-
 # import libraries - not sure what they do
 # used to ensure halo catalog loads properly
 import tempfile
 import shutil
 import os
-
 # Create temporary directory for storing files
 tmpdir = tempfile.mkdtemp()
-
-# import halo catalogue func
+# import halo catalogue func and cosmology func
 from yt.analysis_modules.halo_analysis.api import *
-
+from yt.utilities.cosmology import Cosmology
 # load halo dataset
-halos_ds = yt.load('./halo_catalogs/catalog/catalog0070_thres160.0.h5')
-
+halos_ds = yt.load(halo_catalog_filename)
 # load raw dataset
-ds = yt.load('~/../../tigress/cen/LG4_2048_40pc/RD0070/redshift0070')
-
+ds = yt.load(dataset_filename)
 # Instantiate a catalog using those two paramter files
 hc = HaloCatalog(halos_ds=halos_ds, output_dir=os.path.join(tmpdir, 'halo_catalog'))
 hc.load()
 
+# load python halo list
+with open('calc_list0070_2000', 'rb') as file1:
+    halo_list = pickle.load(file1)
+    count = len(halo_list)
+
+# load redshift and Omega values from parameter file
+with open(redshift_filename, 'rt') as param_file:
+    param_contents = param_file.read()
+    
+    #redshift
+    cindex1 = param_contents.find('CosmologyCurrentRedshift')
+    cindex_eq = param_contents.find('=', cindex1)
+    cindex2 = param_contents.find('\n', cindex_eq, cindex_eq + 100)
+    redshift = float(param_contents[cindex_eq+2:cindex2])
+    
+    # omega_m
+    cindex1 = param_contents.find('CosmologyOmegaMatterNow')
+    cindex_eq = param_contents.find('=', cindex1)
+    cindex2 = param_contents.find('\n', cindex_eq, cindex_eq + 100)
+    omega_m = float(param_contents[cindex_eq+2:cindex2])
+    
+    # hubble const now
+    cindex1 = param_contents.find('CosmologyHubbleConstantNow')
+    cindex_eq = param_contents.find('=', cindex1)
+    cindex2 = param_contents.find('\n', cindex_eq, cindex_eq + 100)
+    hubb_now = float(param_contents[cindex_eq+2:cindex2]) *(u.km / u.s / u.Mpc) 
+    
+    # cosmological constant
+    cindex1 = param_contents.find('CosmologyOmegaLambdaNow')
+    cindex_eq = param_contents.find('=', cindex1)
+    cindex2 = param_contents.find('\n', cindex_eq, cindex_eq + 100)
+    cos_const = float(param_contents[cindex_eq+2:cindex2])
+    
+    # omega baryon as specified by Renyue
+    omega_b = 0.048
+    
+    # box size
+    cindex1 = param_contents.find('CosmologyComovingBoxSize')
+    cindex_eq = param_contents.find('=', cindex1)
+    cindex2 = param_contents.find('\n', cindex_eq, cindex_eq + 100)
+    box_size = float(param_contents[cindex_eq+2:cindex2]) * u.Mpc
+    
+    # left edge strings
+    cindex1 = param_contents.find('RefineRegionLeftEdge')
+    cindex_eq = param_contents.find('=', cindex1)
+    cindex2 = param_contents.find('\n', cindex_eq, cindex_eq + 100)
+    left_edge_string = param_contents[cindex_eq+2:cindex2]
+    left_edges = left_edge_string.split()
+    
+    # right edge strings
+    cindex1 = param_contents.find('RefineRegionRightEdge')
+    cindex_eq = param_contents.find('=', cindex1)
+    cindex2 = param_contents.find('\n', cindex_eq, cindex_eq + 100)
+    right_edge_string = param_contents[cindex_eq+2:cindex2]
+    right_edges = right_edge_string.split()
+    
+# calculate hubble const for simulation
+hubb_z = (100* hubb_now.to('s**-1')) * ((omega_m * (1 + redshift)**3) + (1 - omega_m))**0.5
+
+# calculate crit density and threshold
+GRAV_CONST = (6.67408e-11 * u.m**3 /(u.kg * u.s**2)).to('cm^3*g^-1*s^-2')
+crit_dens = (3 * hubb_z**2) / (8 * pi * GRAV_CONST)
+omegas = (1 - (omega_b / omega_m))
+threshold = 200 * omegas * crit_dens
+
 # specify boundaries of zoom-in box
 # scaling factor multiplied by info from text file 
 # units in cm
-scaling = 2.22535525e+25 # scales dataset coords to cm
+scaling =  ((box_size / hubb_now.value) / (1 + redshift)).to('kpc') # size of box
+xmin = scaling*float(left_edges[0])
+ymin = scaling*float(left_edges[1])
+zmin = scaling*float(left_edges[2])
+xmax = scaling*float(right_edges[0])
+ymax = scaling*float(right_edges[1])
+zmax = scaling*float(right_edges[2])
 
-xmin = scaling*0.39319589 * u.cm
-ymin = scaling*0.42984636 * u.cm
-zmin = scaling*0.41706725 * u.cm
+# store new halo info
+new_halo_list = []
 
-xmax = scaling*0.56298484 * u.cm
-ymax = scaling*0.55089246 * u.cm
-zmax = scaling*0.56698254 * u.cm
-
-'''
-# load radius threshold
-# 200 times (the mean density of the universe times
-# (1-Omega_Baryon/Omega_matter))
-with open('rad_threshold0070_2', 'rb') as infile:
-    threshold = pickle.load(infile)
-'''
-# use threshold as specified by Renyue
-threshold = 200 * 5.92e-28 * (u.g / (u.cm ** 3))
-
-# find ratio of computed and catalog radii
-radii_ratio = []
-
-for halo in hc.halo_list:    
-    # find coord of halo
-    x = halo.quantities.get('particle_position_x') * u.cm
-    y = halo.quantities.get('particle_position_y') * u.cm
-    z = halo.quantities.get('particle_position_z') * u.cm
-    center = [x.value/scaling, y.value/scaling, z.value/scaling]
+# main loop
+for halo in halo_list:
+    # find parameters of halo
+    index = halo[0]
+    x = halo[2]
+    y = halo[3]
+    z = halo[4]
+    center = [(x/scaling).value, (y/scaling).value, (z/scaling).value]
+    halo_mass = halo[5] # new mass
+    radius = halo[7] # new radius
+    isSatellite = halo[9]
     
     # check if halo is inside zoom-in box
     if xmin <= x < xmax and ymin <= y < ymax and zmin <= z < zmax:
         pass
     else:
+        print('OutOfBounds')
+        continue
+
+    # check that radius is not 0
+    if radius == 0:
+        # print reason
+        print('RadiusEqualZero')
+        
+        # ensure that running this code multiple times doesn't create a long list
+        # check if halo already has more info
+        if len(halo) > end_num_entries:
+            # create copy and replace values
+            new_halo = halo
+            new_halo[start_num_entries] = 0
+        else:
+            # append 0's to halo_info if data not applicable
+            new_halo = halo[:start_num_entries]
+            new_halo.append(0)
+
+        # append halo_info to new halo list
+        new_halo_list.append(new_halo)
+        # skip rest of tests
+        continue
+        
+    # check that not a satellite 
+    if isSatellite >= 0:
+        # print reason
+        print('IsSatellite')
+        
+        # ensure that running this code multiple times doesn't create a long list
+        # check if halo already has more info
+        if len(halo) > end_num_entries:
+            # create copy and replace values
+            new_halo = halo
+            new_halo[start_num_entries] = 0
+        else:
+            # append 0's to halo_info if data not applicable
+            new_halo = halo[:start_num_entries]
+            new_halo.append(0)
+
+        # append halo_info to new halo list
+        new_halo_list.append(new_halo)
+        # skip rest of tests
         continue
     
-    # find values of quantities
-    radius = halo.quantities.get('virial_radius') * u.cm
-    rad_val = radius.value
     
-    # create a sphere around halo
-    sp1 = ds.sphere(center, (10*rad_val, 'cm'))
-    rad_min, rad_max = 2e21, 10*rad_val
-    
-    # create radial density profile
-    rp = yt.create_profile(sp1, 'radius', 'density', accumulation=True, 
-                           units = {'radius': 'cm'}, 
-                           logs = {'radius': True, 'density': True}, 
-                           n_bins = 64, 
-                           extrema = {'radius': (rad_min, rad_max)})
-    
-    # find max radius where density > threshold
-    req_rad = rp.x[rp['density'] > threshold.value]
-    if req_rad.size > 0:
-        # find ratio and append to list
-        calc_rad = req_rad[-1]
-        ratio = calc_rad.value / rad_val
-        
-        radii_ratio.append(ratio)
-    else:
-        radii_ratio.append(0)
-
-# store list to file
-with open('rad_ratiolist0070_2', 'wb') as outfile:
-    pickle.dump(radii_ratio, outfile)
