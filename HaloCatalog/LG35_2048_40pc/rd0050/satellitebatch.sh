5621a84f566c5f6025c3ab3a83978b28a9b370af
Yashodhar Govil <ygovil@princeton.edu>
2017-07-22 16:41:35 -0400

    start finding satellites for LG35

---
+++ HaloCatalog/LG35_2048_40pc/rd0050/satellitebatch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -J satellitefinderLG35.0050
+#SBATCH --mail-user=ygovil@princeton.edu
+#SBATCH --mail-type=ALL
+#SBATCH -t 00:59:00
+#SBATCH --mem=4G
+
+#run the application:
+srun -n 1 python ./satellitefinder.py
