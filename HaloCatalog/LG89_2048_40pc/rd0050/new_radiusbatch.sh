abc058b7fc0079a82cda1275524d1cc1aa504f31
Yashodhar Govil <ygovil@princeton.edu>
2017-07-22 16:21:00 -0400

    found new radii for all snapshots

---
+++ HaloCatalog/LG89_2048_40pc/rd0050/new_radiusbatch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -J new_radiusfinderLG89.0050
+#SBATCH --mail-user=ygovil@princeton.edu
+#SBATCH --mail-type=ALL
+#SBATCH -t 08:59:00
+#SBATCH --mem=20G
+
+#run the application:
+srun -n 1 python ./new_radiusfinder.py
