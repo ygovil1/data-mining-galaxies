bae385e24b1d8868b0f6202893dc1bf5f773f74b
Yashodhar Govil <ygovil@princeton.edu>
2017-07-27 16:58:36 -0400

    add and run code to find chi for LG89

---
+++ HaloCatalog/LG89_2048_40pc/rd0030/stel_radbatch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -J stelradfinderLG89.0030
+#SBATCH --mail-user=ygovil@princeton.edu
+#SBATCH --mail-type=ALL
+#SBATCH -t 1:59:00
+#SBATCH --mem=20G
+
+#run the application:
+srun -n 1 python ./stel_radfinder.py
