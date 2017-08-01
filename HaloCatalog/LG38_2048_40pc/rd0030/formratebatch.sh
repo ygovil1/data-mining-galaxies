f6422611f6161af7e30b1cf8e6a8a53ef7500b35
Yashodhar Govil <ygovil@princeton.edu>
2017-08-01 14:50:39 -0400

    add and run code to find formation rate in LG38

---
+++ HaloCatalog/LG38_2048_40pc/rd0030/formratebatch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -J formratefinderLG38.0030
+#SBATCH --mail-user=ygovil@princeton.edu
+#SBATCH --mail-type=ALL
+#SBATCH -t 1:59:00
+#SBATCH --mem=20G
+
+#run the application:
+srun -n 1 python ./formratefinder.py
